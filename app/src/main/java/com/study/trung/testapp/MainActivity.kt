d1104bd3f4ac0c1987f49ee0f25f79bdfa0c7c78
phtrungit <32809538+phtrungit@users.noreply.github.com>
2018-12-11 21:48:46 +0700

    Update MainActivity.kt

--- app/src/main/java/com/study/trung/testapp/MainActivity.kt
+++ app/src/main/java/com/study/trung/testapp/MainActivity.kt
@@ -1,15 +1,9 @@
 package com.study.trung.testapp
 
 import android.app.Activity
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.view.View.OnClickListener
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
-import java.io.File
 
 class MainActivity : Activity() {
     var so1:Int =0
@@ -32,26 +26,18 @@ class MainActivity : Activity() {
         override fun onClick(arg0:View) {
             so1=txtso1.text.toString().toInt()
             so2=txtso2.text.toString().toInt()
-          if(arg0==btnCong)
-          {
-              Kq =so1+so2
-          }
-            else if (arg0==btnTru)
-          {
-              Kq =so1-so2
-          }
-          else if (arg0==btnNhan)
-          {
-              Kq =so1*so2
-          }
-          else if (arg0==btnChia)
-          {
-              if(so2==0)
-                  flag_div0=true
-              else
-                Kq =so1/so2
-
-          }
+            when (arg0)
+            {
+                btnCong -> Kq =so1+so2
+                btnTru -> Kq =so1-so2
+                btnNhan -> Kq =so1*so2
+                btnChia -> {
+                    if(so2==0)
+                        flag_div0=true
+                    else
+                        Kq =so1/so2
+                }
+            }
             if (flag_div0==true)
                 txtKetQua.setText("Lỗi không thể chia cho 0")
             else
